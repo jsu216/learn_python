6bf01ab68b385352dab6ba5e10cc59ce8ed6f799
John Su <haijohn.su@cibc.com>
2020-09-15 20:31:09 -0400

    Add both absolute and relative path examples

--- excercises/files/readfile.py
+++ excercises/files/readfile.py
@@ -1,3 +1,9 @@
-test_file = open('myfile.txt')
+# Use absolute path
+test_file = open(file='/Users/bluejays/Documents/projects/jsu216/learn_python/README.md')
+text = test_file.read()
+print(text)
+
+# Use relative path
+test_file = open(file='../../README.md')
 text = test_file.read()
 print(text)
