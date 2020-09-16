8d180c41f4619dad35c85e9cbfd5f40725246693
John Su <haijohn.su@cibc.com>
2020-09-15 21:03:12 -0400

    Add absolute and relative path to writefile.py examples

--- excercises/files/writefile.py
+++ excercises/files/writefile.py
@@ -1,2 +1,7 @@
+# Use relative path
 test_file = open('myfile2.txt', 'w')
-test_file.write('this is my second text file!')
+test_file.write('this is my second text file line #1!')
+
+# Use absolute path
+test_file = open('/Users/bluejays/Documents/projects/jsu216/learn_python/excercises/files/myfile2.txt', 'a')
+test_file.write('\nthis is my second text file line #2!')
